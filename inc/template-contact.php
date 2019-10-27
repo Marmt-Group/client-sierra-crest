38e58452b4b0218f752e66de62d03858bbed150d
David Davis <davidjamesdavis.djd@gmail.com>
2019-10-27 14:17:47 -0700

    mods to contact

--- inc/template-contact.php
+++ inc/template-contact.php
@@ -30,6 +30,7 @@ get_header('secondary');
                         <div class="wprt-spacer" data-desktop="100" data-mobi="60" data-smobi="60"></div>
                         <div class="wprt-spacer" data-desktop="100" data-mobi="60" data-smobi="60"></div>
                         <div class="wprt-spacer" data-desktop="100" data-mobi="60" data-smobi="60"></div>
+                        <div class="wprt-spacer" data-desktop="100" data-mobi="60" data-smobi="60"></div>
 
                         <div class="container">
                             <div class="row">
